0ddefea3ff85ce286c76bbe972a05ec55269f0f0
Unknown <ty13946@my.bristol.ac.uk>
2021-01-07 18:24:21 +0000

    Tidied tests slightly

--- tests/test_run_autode.py
+++ tests/test_run_autode.py
@@ -2,21 +2,15 @@ from gaptrain.systems import System
 from gaptrain.molecules import Molecule
 from gaptrain import GTConfig
 from gaptrain import Data
-import pytest
 import os
-from autode.methods import ORCA
 from autode.wrappers.keywords import GradientKeywords
-from autode.methods import XTB
 
 here = os.path.abspath(os.path.dirname(__file__))
-h2o = Molecule(os.path.join(here, 'data', 'h2o.xyz'))
+h2o = Molecule(os.path.join(here, 'data', 'h2co.xyz'))
 GTConfig.orca_keywords = GradientKeywords(['PBE', 'def2-SVP', 'EnGrad'])
 
 
 def test_autode_orca():
-
-    orca_calculator = ORCA()
-    assert isinstance(orca_calculator, ORCA)
 
     if 'GT_ORCA' not in os.environ or not os.environ['GT_ORCA'] == 'True':
         return
@@ -26,14 +20,13 @@ def test_autode_orca():
     water.add_molecules(h2o, n=2)
 
     config = water.random()
-
     config.run_orca()
+
+    assert config.energy is not None
+    assert config.forces is not None
 
 
 def test_autode_xtb():
-
-    xtb_calculator = XTB()
-    assert isinstance(xtb_calculator, XTB)
 
     if 'GT_XTB' not in os.environ or not os.environ['GT_XTB'] == 'True':
         return
@@ -43,14 +36,13 @@ def test_autode_xtb():
     water.add_molecules(h2o, n=5)
 
     config = water.random()
-
     config.run_xtb()
+
+    assert config.energy is not None
+    assert config.forces is not None
 
 
 def test_autode_parallel_orca():
-
-    orca_calculator = ORCA()
-    assert isinstance(orca_calculator, ORCA)
 
     if 'GT_ORCA' not in os.environ or not os.environ['GT_ORCA'] == 'True':
         return
@@ -58,20 +50,15 @@ def test_autode_parallel_orca():
     # Check that orca runs work
     water = System(box_size=[10, 10, 10])
     water.add_molecules(h2o, n=2)
-    monomer = water.random()
 
     configs = Data()
     for _ in range(3):
-        monomer_pert = monomer.copy()
-        monomer_pert.add_perturbation(sigma=0.05, max_length=1)
-        configs += monomer_pert
+        configs += water.random(with_intra=True)
 
     configs.parallel_orca()
 
 
 def test_autode_parallel_xtb():
-    xtb_calculator = XTB()
-    assert isinstance(xtb_calculator, XTB)
 
     if 'GT_XTB' not in os.environ or not os.environ['GT_XTB'] == 'True':
         return
@@ -79,12 +66,10 @@ def test_autode_parallel_xtb():
     # Check that xtb runs work
     water = System(box_size=[10, 10, 10])
     water.add_molecules(h2o, n=2)
-    monomer = water.random()
+    water.add_molecules(h2o, n=2)
 
     configs = Data()
     for _ in range(3):
-        monomer_pert = monomer.copy()
-        monomer_pert.add_perturbation(sigma=0.05, max_length=1)
-        configs += monomer_pert
+        configs += water.random(with_intra=True)
 
     configs.parallel_xtb()
